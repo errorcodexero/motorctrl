21e8fb660f67ec3acb98fb1990e16852810fbef7
Jack (Butch) Griffin <butchg@comcast.net>
2022-01-24 06:40:48 -0800

    added color sensor test subsystem for electrical testing

--- src/main/java/frc/robot/MotorCtrlRobotSubsystem.java
+++ src/main/java/frc/robot/MotorCtrlRobotSubsystem.java
@@ -5,12 +5,16 @@ import org.xero1425.base.XeroRobot;
 
 public class MotorCtrlRobotSubsystem extends RobotSubsystem {
     TestMotorSubsystem sub_ ;
+    ColorSensorSubsystem color_ ;
 
     public MotorCtrlRobotSubsystem(XeroRobot robot) throws Exception {
         super(robot, "MotorCtrlSubsystem") ;
 
         sub_ = new TestMotorSubsystem(this) ;
         addChild(sub_);
+
+        color_ = new ColorSensorSubsystem(this, "color") ;
+        addChild(color_) ;
     }
 
     public TestMotorSubsystem getTestmotors() {
